1696fc58faf749cb7608cc3483a724da26e3635f
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2018-11-12 14:34:10 -0800

    working on game-form

--- js/game-form.js
+++ js/game-form.js
@@ -1,7 +1,17 @@
-// import html from './html.js';
+import html from './html.js';
 
-// function makeTemplate () {
-//     return html`
-//         <input=""
-//     `;
-// }
+function makeTemplate () {
+    return html`
+        <div id= answers>
+        <input required type="radio" id="genie in a bottle" value="genie in a bottle">
+        <label for="genie in a bottle"> Genie in a Bottle</label>
+        </div>
+    `;
+}
+
+export default class Answers 
+render() {
+    const dom = makeTemplate();
+
+    return dom;
+}
