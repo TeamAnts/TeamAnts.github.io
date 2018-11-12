4fa355df1213dcc801d033c3a5dd60adc8221255
Aaron Dennis <AMDennis1987@gmail.com>
2018-11-12 15:44:31 -0800

    added footer and layout

--- js/header.js
+++ js/header.js
@@ -2,13 +2,7 @@ import html from './html.js';
 
 function makeTemplate() {
     return html`
-        <header>
             <h1> NAME THAT <span>TUNE</span> </h1>
-            <h1>&#9833;</h1>
-            <h1>&#9835;</h1>
-            <h1>&#9833;</h1>
-            <h1>&#9834;</h1>
-            </header>
     `;
 }
 
