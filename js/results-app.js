0875b79889899c1f2f226365dc716205bb2ec4e0
Carmen V Ramos <carmenvramos@yahoo.com>
2018-11-14 14:33:27 -0800

    Added Header & Footer to Results Page, added styling

--- js/results-app.js
+++ js/results-app.js
@@ -1,5 +1,7 @@
 import html from './html.js';
 import playersApi from './players-api.js';
+import Header from './header.js';
+import Footer from './footer.js';
 import Results from './results.js';
 import ResultTable from './results-table.js';
 
@@ -14,7 +16,9 @@ function makeTemplate() {
               <h2>TOP 10 PLAYERS</h2>
               
           </section>
-      </main>`;
+      </main>
+      <footer></footer>
+      `;
 }
 
 class ResultsApp {
@@ -25,19 +29,23 @@ class ResultsApp {
     render() {
         
         const dom = makeTemplate();
+
+        const headerContainer = dom.querySelector('header');
+        const header = new Header;
+        headerContainer.appendChild(header.render());
   
         const playerScoreSection = dom.querySelector('.player-score');
-        const topTenSection = dom.querySelector('.top-ten');
-        
         const result = new Results(this.currentPlayer);
-
-      
         playerScoreSection.appendChild(result.render());
-  
+        
+        const topTenSection = dom.querySelector('.top-ten');
         const results = new ResultTable(this.results);
         topTenSection.appendChild(results.render());
   
-  
+        const footerContainer = dom.querySelector('footer');
+        const footer = new Footer;
+        footerContainer.appendChild(footer.render());
+        
         return dom;
     }
 }
