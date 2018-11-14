56dbe0356540d0a61c9131aaa8b9a71a68233a34
‘Michael <pickup.patterns@gmail.com>
2018-11-14 10:38:27 -0800

    top-ten sorts highest to lowest

--- js/results-table.js
+++ js/results-table.js
@@ -6,20 +6,32 @@ function makeTemplate() {
      <ul class="top-ten-list"></ul>
 `;
 }
+
 export default class ResultsTable{
     constructor(results) {
         this.results = results;
     }
 
+
     render() {
         const dom = makeTemplate();
         const list = dom.querySelector('.top-ten-list');
 
-        this.results.forEach(player =>{
+        let sortedArray = this.results.sort(function(a, b) {
+            return b.score - a.score;
+    
+
+        });
+        sortedArray.forEach(player =>{
             const resultsTableItem = new ResultsTableItem(player);
             list.appendChild(resultsTableItem.render());
+
+           
         
         });
+        
+
+        console.log(sortedArray);
         return dom;
         
     }
